ebf57c12763ff01d36745124b675de42377c66ce
Carlos Henrique Damasceno <carlos_henrique1313@outlook.com>
2023-05-05 11:58:00 -0300

    change email to a value object

---
+++ src/main/java/hexagonal/user/Email.java
@@ -0,0 +1,20 @@
+package hexagonal.user;
+
+import hexagonal.exceptions.BusinessException;
+
+public final class Email {
+    private final String value;
+
+    public Email(String email) {
+        String emailRegex = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$";
+        if (!email.matches(emailRegex)) {
+            throw new BusinessException("Email inválido");
+        }
+        value = email;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+}
--- src/main/java/hexagonal/user/User.java
+++ src/main/java/hexagonal/user/User.java
@@ -3,13 +3,13 @@ package hexagonal.user;
 import hexagonal.adapters.EncryptorAdapter;
 
 public class User {
-    private Long id;
-    private String name;
-    private String username;
-    private String email;
-    private Password password;
+    private final Long id;
+    private final String name;
+    private final String username;
+    private final Email email;
+    private final Password password;
 
-    private User(Long id, String name, String username, String email, Password password) {
+    private User(Long id, String name, String username, Email email, Password password) {
         this.id = id;
         this.name = name;
         this.username = username;
@@ -28,52 +28,33 @@ public class User {
                 null,
                 name,
                 username,
-                email,
+                new Email(email),
                 Password.generateEncrypted(nonEncryptedPassword, encryptorAdapter)
         );
     }
 
     public static User buildExistentUser(Long id, String name, String username, String email, String password) {
-        return new User(id, name, username, email, new Password(password));
+        return new User(id, name, username, new Email(email), new Password(password));
     }
 
     public Long getId() {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public String getUsername() {
         return username;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getEmail() {
+    public Email getEmail() {
         return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
     }
 
     public Password getPassword() {
         return password;
     }
 
-    public void setPassword(Password password) {
-        this.password = password;
-    }
 }
